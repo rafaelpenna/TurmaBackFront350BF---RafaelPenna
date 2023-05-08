ded1ef280c27a98b739e1fb803d205f163c60200
Rafael Penna <ragapen@gmail.com>
2023-05-08 17:28:59 -0300

    finalização da criação do app utilizando notification center. Envio de dados entre telas utilizando Observer.

--- NotificationCenterProtocol/NotificationCenterProtocol/ViewController.swift
+++ NotificationCenterProtocol/NotificationCenterProtocol/ViewController.swift
@@ -12,14 +12,40 @@ class ViewController: UIViewController {
     @IBOutlet var appleLogoImage: UIImageView!
     @IBOutlet var textLabel: UILabel!
     @IBOutlet var clickButton: UIButton!
-
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        configAtributes()
+        configObserver()
+    }
+    
+    private func configAtributes() {
+        appleLogoImage.image = UIImage(named: "Image")
+        textLabel.text = "Apple"
+    }
+    
+    func configObserver() {
+        NotificationCenter.default.addObserver(self, selector: #selector(updateMacbook), name: .macbook, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(updateiMac), name: .imac, object: nil)
+    }
+    
+    @objc func updateMacbook(_ notification: NSNotification) {
+        appleLogoImage.image = UIImage(named: "Image-3")
+        textLabel.text = "MacBook Pro"
+        view.backgroundColor = notification.object as? UIColor
+    }
+    
+    @objc func updateiMac(_ notification: NSNotification) {
+        appleLogoImage.image = UIImage(named: "Image-1")
+        textLabel.text = "iMac Pro"
+        view.backgroundColor = .yellow
     }
 
     @IBAction func tappedButton(_ sender: UIButton) {
+        let vc = UIStoryboard(name: "SelectionViewController", bundle: nil).instantiateViewController(withIdentifier: "SelectionViewController") as? SelectionViewController
+        vc?.modalPresentationStyle = .fullScreen
+        present(vc ?? UIViewController(), animated: true)
+        
     }
     
 }
