071bb4c1ab7e45f5ada6d985e93ba563f84825d1
Rafael Penna <ragapen@gmail.com>
2023-05-08 13:44:18 -0300

    criado app base para notification center

---
+++ NotificationCenterProtocol/NotificationCenterProtocol/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  NotificationCenterProtocol
+//
+//  Created by Rafael Penna on 04/05/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var appleLogoImage: UIImageView!
+    @IBOutlet var textLabel: UILabel!
+    @IBOutlet var clickButton: UIButton!
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func tappedButton(_ sender: UIButton) {
+    }
+    
+}
+
